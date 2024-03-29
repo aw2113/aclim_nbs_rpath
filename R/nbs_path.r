9b68c2581ade29a509dbe674862680e650ba96c6
Andy Whitehouse <andy.whitehouse@noaa.gov>
2024-03-29 15:05:43 -0700

    Added a vector of juvenile group names.

--- R/nbs_path.r
+++ R/nbs_path.r
@@ -5,13 +5,16 @@ source("rpath_graphs.R")
 
 Ebase <- "model/nbs_2010_base.csv"  # Base biomass, production, fishing, etc.
 Ediet <- "model/nbs_2010_diet.csv"  # Diet matrix
-# Eped  <- "model/nbs_2010_pedigree.csv"   # Data pedigree = quality of input data
+Eped  <- "model/nbs_2010_pedigree.csv"   # Data pedigree = quality of input data
 Estz  <- "model/nbs_stanzas.csv"   # Stanzas
 Estg  <- "model/nbs_stanza_groups.csv" # Stanza groups
 
 # Setup Base Ecopath params
-unbal.params <- read.rpath.params(Ebase, Ediet, Estg, Estz) # unbalanced
+unbal.params <- read.rpath.params(Ebase, Ediet, Eped, Estg, Estz) # unbalanced
 # unbalanced Ecopath
 unbal <- rpath.stanzas(unbal.params)
 # try to balance
 bal <- rpath(unbal) # balanced
+
+juv_groups <- c("pollock_juv","pcod_juv","arrowtooth_juv","gr_turbot_juv",
+                "halibut_juv")
